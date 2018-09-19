4f494bf3987792664cadcea9a6cbfb37015f9e9d
ARTECH Consultancy Ltd <42694465+artechconsultancyltd@users.noreply.github.com>
2018-09-19 20:42:57 +0100

    Delete upl.php

--- upl.php
+++
@@ -1,4 +0,0 @@
-<?php 
-//make sure this file is dropped against the client root directory
-move_uploaded_file($_FILES["file"]["tmp_name"], "sites/default/files/civicrm/custom/" . $_FILES["file"]["name"]); 
-?> 
